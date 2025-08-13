594d703bba4fc906db810504cb8e556fc8b700db
Haya Feng <wswangyue@yeah.net>
2025-08-13 08:45:11 +0800

    safe20

--- contracts/factory/StakingPool.sol
+++ contracts/factory/StakingPool.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 
 contract StakingPool is Ownable, Initializable{
